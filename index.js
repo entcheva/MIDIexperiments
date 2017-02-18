6814f0ec536160ec4d20f5cea925ba3a90bf6f01
entcheva <christina.entcheva@gmail.com>
2017-02-18 17:00:33 -0500

    Map keypress to P5 shape

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-
-
+// keyboard
 document.addEventListener('keydown', keyboard, false)
-
 function keyboard(key) {
 	if (key.which === 81) {
     var audio = document.getElementById('a1')
@@ -42,5 +40,28 @@ function keyboard(key) {
   } else if (key.which === 220) {
     var audio = document.getElementById('g1s')
     audio.play()
+  }
+}
+
+
+// begin p5
+function setup() {
+  createCanvas(600,600)
+}
+
+
+var value = 0
+var c = color(255, 204, 0)
+
+function draw() {
+  fill(value)
+  rect(30, 30, 50, 50)
+}
+
+function keyPressed() {
+  if (value === 0) {
+    value = 255
+  } else {
+    value = 0
   }
 }
